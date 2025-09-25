92ed012463edb08a8da5f6b20c1e8e8771c92a76
Luther <vale@luther-online.de>
2025-09-25 14:39:13 +0200

    • OrderHeader: Navigation zu ApplicationUser per Konvention (FK ApplicationUserId) • Entfernt falsches [ForeignKey("ApplicationUser")] Attribut causing design-time DbContext error • Felder für Versand, Zahlung und Summen angelegt (OrderTotal, PaymentDate, PaymentDueDate etc.)

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,93 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ShoppingCartVM = new()
+            {
+                ShoppingcartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+
+            };
+            foreach(var cart in ShoppingCartVM.ShoppingcartList)
+            {
+                cart.Price = GetPriceBasedOnQuantitiy(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+            return View(ShoppingCartVM);
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+        
+        public IActionResult Minus(int cardId)
+        {
+            ShoppingCart shoppingCart = _unitOfWork.ShoppingCart.Get(u => u.Id == cardId);
+            if(shoppingCart.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(shoppingCart);
+            }
+            else
+            {
+                shoppingCart.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(shoppingCart);
+
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Plus(int cardId)
+        {
+            ShoppingCart shoppingCart = _unitOfWork.ShoppingCart.Get(u => u.Id == cardId);
+            shoppingCart.Count += 1;
+            _unitOfWork.ShoppingCart.Update(shoppingCart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Remove(int cardId)
+        {
+            ShoppingCart shoppingCart = _unitOfWork.ShoppingCart.Get(u => u.Id == cardId);
+            _unitOfWork.ShoppingCart.Remove(shoppingCart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private double GetPriceBasedOnQuantitiy(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
+        }
+    }
+}
